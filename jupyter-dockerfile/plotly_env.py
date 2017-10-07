d9efe9881d56d905e3439816b9073298841ae9de
loujine <loujine@protonmail.com>
2017-10-07 18:52:36 +0000

    Add plotly helper

---
+++ jupyter-dockerfile/plotly_env.py
@@ -0,0 +1,9 @@
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+
+# offline mode for plot.ly, no need to store the graphs in their cloud
+from plotly.offline import iplot
+
+# special mode to display plotly graphs inside Jupyter notebooks
+from plotly.offline import init_notebook_mode
+init_notebook_mode()
