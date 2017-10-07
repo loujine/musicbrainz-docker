6c9edf0785f3b6b8f6c178dc67f6607955c8d156
loujine <loujine@protonmail.com>
2017-10-07 15:32:00 +0000

    Create JupyterHub VM

---
+++ jupyterhub-dockerfile/jupyterhub_config.py
@@ -0,0 +1,18 @@
+#  See `hub_connect_ip` for cases where the bind and connect address should
+#  differ.
+c.JupyterHub.hub_ip = '127.0.0.1'
+
+## The public facing port of the proxy
+c.JupyterHub.port = 8000
+
+## Select the Proxy API implementation.
+c.JupyterHub.proxy_class = 'jupyterhub.proxy.ConfigurableHTTPProxy'
+
+## The class to use for spawning single-user servers.
+c.JupyterHub.spawner_class = 'jupyterhub.spawner.LocalProcessSpawner'
+
+## The IP address (or hostname) the single-user server should listen on.
+c.Spawner.ip = '127.0.0.1'
+
+#  Defaults to an empty set, in which case no user has admin access.
+c.Authenticator.admin_users = set(['loujine'])
